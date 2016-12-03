8bfd06f832a118d1cdf587c0a8f6e153c31b23cc
SeihakRithy Muth <Rithy58@gmail.com>
2016-12-03 11:26:11 -0500

    added depends and single test loader

--- src/main/java/major/mutation/plugin/singleTestLoader.java
+++ src/main/java/major/mutation/plugin/singleTestLoader.java
@@ -3,20 +3,32 @@ package major.mutation.plugin;
 import org.junit.runners.model.TestClass;
 
 public class singleTestLoader implements TestSuiteBuilder {
+
+  private String className;
+  private TestClass testSuite;
+
+  public singleTestLoader(String name) {
+    className = name;
+  }
+
+  public void setClassName(String name) {
+    className = name;
+  }
+
   // load the Test Suite and return true if successful
   public boolean loadTest() {
-
+    // TODO: Load the test, return false if test not found
     return true;
   }
 
   // set up the Test Suite
   public void setupTest() {
-
+    //probably doesn't need to setup the test for this
   }
 
   // return the TestSuite to be run
   public TestClass getTestSuite() {
 
-    return null;
+    return testSuite;
   }
 }
