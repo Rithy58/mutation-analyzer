bf173813cbc1ee69767a59caedb15a7d74361e04
SeihakRithy Muth <Rithy58@gmail.com>
2016-12-07 12:06:05 -0500

    Refactor CoverageRunner and Listener

--- src/main/java/major/mutation/plugin/CoverageRunner.java
+++ src/main/java/major/mutation/plugin/CoverageRunner.java
@@ -4,7 +4,9 @@ import org.junit.runners.model.TestClass;
 import org.junit.runner.Result;
 import org.junit.runner.JUnitCore;
 import java.util.*;
-
+import org.junit.runner.notification.RunListener;
+import org.junit.runner.Description;
+import major.mutation.Config;
 
 public class CoverageRunner implements TestSuiteRunner{
   // load the Test Suite
@@ -33,5 +35,38 @@ public class CoverageRunner implements TestSuiteRunner{
   // return the TestSuite to be run
   public int getResult() {
     return result;
+  }
+
+  private class CoverageListener extends RunListener {
+
+  	HashMap<String, List<Integer>> coverage = new HashMap<String, List<Integer>>();
+
+  	public void testRunStarted (Description description) {
+  		Config.__M_NO = 0;
+  	}
+
+  	public void testStarted (Description description) {
+  		Config.reset();
+  	}
+
+  	public void testFinished (Description description) {
+  		List<Integer> covered = new ArrayList<Integer>();
+  		covered = Config.getCoverageList();
+  		coverage.put(description.getMethodName(), covered);
+
+  		/*System.out.print(description.getMethodName() + ":");
+  		List<Integer> l = Config.getCoverageList();
+
+  		for(Integer mut : l){
+  			System.out.print(" " + mut);
+  		}
+  		System.out.println("\n");*/
+  	}
+
+  	public HashMap<String, List<Integer>> getCoverage(){
+  		return coverage;
+  	}
+
+
   }
 }
