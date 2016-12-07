a044057922de0d7ddc27a309fd9e5252c8d2727c
Ethan Liu <darthpikakirby@gmail.com>
2016-12-06 22:52:48 -0500

    Added CoverageListener

---
+++ src/main/java/major/mutation/plugin/CoverageListener.java
@@ -0,0 +1,12 @@
+package major.mutation.plugin;
+
+import org.junit.runner.notification.RunListener;
+import org.junit.runner.Description;
+
+public class CoverageListener extends RunListener {
+
+	public void testFinished(Description description){
+
+	}
+
+}
