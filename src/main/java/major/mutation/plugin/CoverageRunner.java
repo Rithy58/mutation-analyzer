222e66b2c422f2f1a4a9b35c90c060b25fc0a756
SeihakRithy Muth <Rithy58@gmail.com>
2016-12-06 22:56:05 -0500

    added JUnitCore

--- src/main/java/major/mutation/plugin/CoverageRunner.java
+++ src/main/java/major/mutation/plugin/CoverageRunner.java
@@ -2,21 +2,29 @@ package major.mutation.plugin;
 
 import org.junit.runners.model.TestClass;
 import org.junit.runner.Result;
+import org.junit.runner.JUnitCore;
 
 
 public class CoverageRunner implements TestSuiteRunner{
   // load the Test Suite
-  public void loadTest(TestClass testSuite) {
+  private int result;
+  private TestClass testClass;
 
+  public void loadTest(TestClass testSuite) {
+    testClass = testSuite;
   }
 
   // run the Test Suite
   public void runTest() {
-
+    JUnitCore junit = new JUnitCore();
+    CoverageListener listener = new CoverageListener();
+    junit.addListener(listener);
+    Result junitResult = junit.run(testClass.getJavaClass());
+    result = junitResult.getRunCount();
   }
 
   // return the TestSuite to be run
   public int getResult() {
-    return -1;
+    return result;
   }
 }
