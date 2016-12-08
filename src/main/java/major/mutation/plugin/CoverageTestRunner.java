b1c55b3cea9645b705b98d8e8cc85bbed9586368
Kenneth Tsui <kjtsui15@gmail.com>
2016-12-07 20:46:01 -0500

    add CoverageTestRunner

---
+++ src/main/java/major/mutation/plugin/CoverageTestRunner.java
@@ -0,0 +1,19 @@
+package major.mutation.plugin;
+
+import org.junit.Test;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.TestClass;
+import org.junit.runners.model.FrameworkMethod;
+import java.util.*;
+
+public class CoverageTestRunner {
+
+  private HashMap<String, List<Integer>> coverage;
+
+  public CoverageTestRunner(HashMap coverage, TestClass testClass) throws InitializationError {
+    super(testClass.getJavaClass());
+    this.coverage = coverage;
+  }
+
+}
