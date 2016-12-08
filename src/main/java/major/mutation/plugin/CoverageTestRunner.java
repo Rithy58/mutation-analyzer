035f73eedecb4f80e9a84573007b5bfe9c3f5983
Ethan Liu <darthpikakirby@gmail.com>
2016-12-07 20:55:31 -0500

    Added getMutants method to CoverageTestRunner

--- src/main/java/major/mutation/plugin/CoverageTestRunner.java
+++ src/main/java/major/mutation/plugin/CoverageTestRunner.java
@@ -10,9 +10,26 @@ import java.util.*;
 public class CoverageTestRunner {
 
   private HashMap<String, List<Integer>> coverage;
+  private TestClass testSuite;
 
   public CoverageTestRunner(HashMap coverage, TestClass testClass) {
     this.coverage = coverage;
+    testSuite = testClass;
   }
+
+ 	public List<Integer> getMutants(){
+		List<Integer> mutants = new ArrayList<Integer>();
+		for(List<Integer> m : coverage.values()){
+			for(int i = 0; i < m.size(); i++){
+				if(!mutants.contains(m.get(i))){
+					mutants.add(m.get(i));
+				}
+			}
+		}
+
+		return mutants;
+	}
+
+
 
 }
