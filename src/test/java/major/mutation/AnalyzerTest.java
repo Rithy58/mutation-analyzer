c9754e025f68054e43db640d825e2b73e2098eb4
SeihakRithy Muth <Rithy58@gmail.com>
2016-12-13 16:49:16 -0500

    added more tests

--- src/test/java/major/mutation/AnalyzerTest.java
+++ src/test/java/major/mutation/AnalyzerTest.java
@@ -1,11 +1,24 @@
-import major.mutation.Analyzer;
+package major.mutation;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
+import major.mutation.Config;
+import major.mutation.Analyzer;
+import major.mutation.plugin.*;
+import major.mutation.plugin.coverage.*;
 
 public class AnalyzerTest {
     @Test
     public void testAnalyzer() {
         Analyzer analyzer = new Analyzer();
         assertNotNull("Analyzer failed to instantiate", analyzer);
+        TestSuiteBuilder testLoader = new MultiTestLoader("");
+        TestSuiteBuilder singleTestLoader = new SingleTestLoader("");
+        TestSuiteRunner testRunner = new CoverageRunner();
+        ScoreCalculator testResult = new MutantKilled();
+
+        analyzer.registerTestSuiteBuilder(testLoader);
+        analyzer.registerTestSuiteRunner(testRunner);
+        analyzer.registerScoreCalculator(testResult);
     }
 }
