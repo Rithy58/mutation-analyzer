d72d82a7a1d0cfc6eab333a60d912b247d17017e
Ethan Liu <darthpikakirby@gmail.com>
2016-12-16 18:59:16 -0500

    patching

--- src/main/java/major/mutation/plugin/coverage/CoverageRunner.java
+++ src/main/java/major/mutation/plugin/coverage/CoverageRunner.java
@@ -4,10 +4,15 @@ import major.mutation.Config;
 import major.mutation.plugin.TestSuiteRunner;
 import major.mutation.plugin.util.*;
 import org.junit.runners.model.TestClass;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.InitializationError;
 import org.junit.runner.*;
 import java.util.*;
 
-public class CoverageRunner implements TestSuiteRunner{
+public class CoverageRunner implements TestSuiteRunner {
+
+  private static final long JOIN_TIMEOUT = 1000;
+
   private int result;
   private TestClass[] testClasses;
   private List<Integer> killedMutants;
@@ -15,14 +20,13 @@ public class CoverageRunner implements TestSuiteRunner{
   // load the Test Suite
   public void loadTest(TestClass[] testSuites) {
     testClasses = testSuites;
-    result = 0;
     killedMutants = new ArrayList<Integer>();
+    result = 0;
   }
 
   // run the Test Suite
   public void runTest() {
     CoverageInformation[] information = new CoverageInformation[testClasses.length];
-    CoverageInformation.reset();
     JUnitCore junit = new JUnitCore();
     for(int i = 0; i < testClasses.length; i++) {
       CoverageListener coverageListener = new CoverageListener();
@@ -40,8 +44,8 @@ public class CoverageRunner implements TestSuiteRunner{
           continue;
         }
         if (killMutant(testClasses[t], information[t], junit)) {
-          result ++;
           killedMutants.add(Config.__M_NO);
+          result ++;
           break;
         }
       }
@@ -51,21 +55,60 @@ public class CoverageRunner implements TestSuiteRunner{
   private boolean killMutant(TestClass testClass, CoverageInformation information, JUnitCore runner) {
     Request request;
     Result result;
+    MethodThread t;
     List<String> methods = information.getSortedKeys();
     for (String testCase : methods) {
       if(!information.get(testCase).contains(Config.__M_NO)) {
         continue;
       }
-      request = Request.method(testClass.getJavaClass(), testCase);
-      result = runner.run(request);
-      if (result.getFailureCount() > 0) {
+      t = new MethodThread(runner, testClass, testCase);
+      t.start();
+      try {
+        // System.out.println(information.getRuntime(testCase));
+        int milliseconds = 0;
+        int deltaRemainder = information.getRuntime(testCase) * 100;
+        if ( deltaRemainder > 999999 ) {
+             milliseconds = deltaRemainder / 1000000;
+             deltaRemainder = deltaRemainder % 1000000;
+        }
+        t.join(milliseconds, deltaRemainder);
+      } catch (Exception e) {
+        // Failed to interrupt
+        t.interrupt();
+      }
+      if (!t.finished || !t.passed) {
         return true;
       }
     }
     return false;
   }
 
+  // return the TestSuite to be run
   public int getResult() {
     return result;
   }
+
+  private static class MethodThread extends Thread {
+    private final JUnitCore junit;
+    private final Class<?> testClass;
+    private final String testMethod;
+    private boolean passed = true;
+    private boolean finished = false;
+
+    public MethodThread(JUnitCore core, TestClass clazz, String method) {
+      junit = core;
+      testClass = clazz.getJavaClass();
+      testMethod = method;
+    }
+
+
+    @Override
+    public void run() {
+      if (junit.run(Request.method(testClass, testMethod)).getFailureCount() > 0) {
+        passed = false;
+      }
+      finished = true;
+    }
+  }
+
 }
