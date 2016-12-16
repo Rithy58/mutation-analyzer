a6bec52fcd4b4d4224658b3f4340a7076a6cac1e
SeihakRithy Muth <Rithy58@gmail.com>
2016-12-15 20:04:21 -0500

    refactored codes

--- src/main/java/major/mutation/plugin/coverage/CoverageOnlyRunner.java
+++ src/main/java/major/mutation/plugin/coverage/CoverageOnlyRunner.java
@@ -10,17 +10,20 @@ import java.util.*;
 public class CoverageOnlyRunner implements TestSuiteRunner {
   private int result;
   private TestClass[] testClasses;
+  private List<Integer> killedMutants;
 
   // load the Test Suite
   public void loadTest(TestClass[] testSuites) {
     testClasses = testSuites;
     result = 0;
+    killedMutants = new ArrayList<Integer>();
   }
 
+  // run the Test Suite
   public void runTest() {
     JUnitCore junit = new JUnitCore();
     CoverageInformation[] information = new CoverageInformation[testClasses.length];
-    Config.__M_NO = 0;
+    CoverageInformation.reset();
     for(int i = 0; i < testClasses.length; i++) {
       CoverageListener coverageListener = new CoverageListener();
       junit.addListener(coverageListener);
@@ -38,28 +41,29 @@ public class CoverageOnlyRunner implements TestSuiteRunner {
         }
         if (killMutant(testClasses[t], information[t], junit)) {
           result ++;
+          killedMutants.add(Config.__M_NO);
           break;
         }
       }
     }
   }
 
-    private boolean killMutant(TestClass testClass, CoverageInformation information, JUnitCore runner) {
-      Request request;
-      Result result;
-      List<String> methods = information.getKeys();
-      for (String testCase : methods) {
-        if(!information.get(testCase).contains(Config.__M_NO)) {
-          continue;
-        }
-        request = Request.method(testClass.getJavaClass(), testCase);
-        result = runner.run(request);
-        if (result.getFailureCount() > 0) {
-          return true;
-        }
+  private boolean killMutant(TestClass testClass, CoverageInformation information, JUnitCore runner) {
+    Request request;
+    Result result;
+    List<String> methods = information.getKeys();
+    for (String testCase : methods) {
+      if(!information.get(testCase).contains(Config.__M_NO)) {
+        continue;
       }
-      return false;
+      request = Request.method(testClass.getJavaClass(), testCase);
+      result = runner.run(request);
+      if (result.getFailureCount() > 0) {
+        return true;
+      }
     }
+    return false;
+  }
 
   public int getResult() {
     return result;
